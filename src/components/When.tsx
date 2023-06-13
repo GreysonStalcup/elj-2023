020c8d4a48188bd0c6ae85afdef0c2ef6e55ff5d
Greyson Stalcup <g_stalcup@hotmail.com>
2023-06-13 09:42:13 -0700

    add info card

--- src/components/When.tsx
+++ src/components/When.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import location from "/location.png";
-
+import info from "/when/info.png";
+import button from "/when/button.png";
+import collage from "/when/collage.png";
 export default function When() {
   return (
-    <div>
-      <img src={location} alt="location" />
-    </div>
+    <motion.div initial={{x: "-100%", opacity: 0}} whileInView={{x: "0", opacity: 1}} transition={{duration: 1}} className='bg-white flex md:w-fit p-4 flex-col md:flex-row mx-auto gap-8 md:gap-0 w-10/12 items-center mt-24'>
+      <div className='flex flex-col items-center'>
+        <div className="flex-initial">
+          <img src={info} alt="info" />
+        </div>
+        <div className="">
+          <a href="#" target='_blank'><img src={button} alt="register button" /></a>
+        </div>
+      </div>
+      <div>
+        <img src={collage} alt="collage" />
+      </div>
+    </motion.div>
   );
 }
