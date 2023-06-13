763258d40a3bdd30c2db4e2c1ac685e44a85bd86
Greyson Stalcup <g_stalcup@hotmail.com>
2023-06-13 12:42:52 -0700

    change fonts

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import invited from "/intro/invited.png";
 import logo from "/intro/sdcbf_logo.png";
 import invitationSponsor from "/intro/invitation_sponsor.png";
+import "../styles/main.css";
 export default function IntroText() {
   return (
     <motion.div
@@ -19,14 +20,14 @@ export default function IntroText() {
           alt="invite text"
         />
       </div>
-      <h1 className="p-10 md:p-0 text-4xl md:text-6xl font-bold">
+      <h1 className="p-10 md:p-0 text-4xl md:text-7xl font-oswald">
         26th Annual An Evening in La Jolla
       </h1>
       <div className="tagline">
         <h2 className="text-xl md:text-3xl uppercase">
           The best legal party in town!
         </h2>
-        <p className="text-sm md:text-base italic">
+        <p className="md:text-5xl font-tangerine text-2xl">
           A benefit that supports San Diego legal charities
         </p>
         <div className="w-screen mx-auto md:float-right md:px-24">
