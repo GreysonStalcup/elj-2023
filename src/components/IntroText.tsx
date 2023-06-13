6984f7e897a8e525f6e4c4e15da75a480b44d314
Greyson Stalcup <g_stalcup@hotmail.com>
2023-06-13 10:44:31 -0700

    style when info card

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import invited from '/intro/invited.png';
-import logo from '/intro/sdcbf_logo.png';
+import React from "react";
+import { motion } from "framer-motion";
+import invited from "/intro/invited.png";
+import logo from "/intro/sdcbf_logo.png";
+import invitationSponsor from "/intro/invitation_sponsor.png";
 export default function IntroText() {
   return (
-    <motion.div initial={{ y: "-100%", opacity: 0 }} whileInView={{ y: "0", opacity: 1 }} transition={{ duration: 2 }}
+    <motion.div
+      initial={{ y: "-100%", opacity: 0 }}
+      whileInView={{ y: "0", opacity: 1 }}
+      transition={{ duration: 2 }}
       className="flex justify-evenly h-full flex-col my-au mx-auto text-center text-white relative z-10"
     >
-      <div className='text-center items-center'>
-        <img src={invited} className="mx-auto" alt='invite text' />
-        <img src={logo} className="mx-auto w-1/2 md:w-1/4" alt='invite text' />
+      <div className="text-center items-center">
+        <img src={invited} className="mx-auto" alt="invite text" />
+        <img src={logo} className="mx-auto w-1/2 md:w-1/4" alt="invite text" />
       </div>
-      <h1 className="p-1 md:p-0 text-4xl md:text-6xl font-bold">
-          26th Annual An Evening in La Jolla
-        </h1>
+      <h1 className="p-10 md:p-0 text-4xl md:text-6xl font-bold">
+        26th Annual An Evening in La Jolla
+      </h1>
       <div className="tagline">
-        
         <h2 className="text-xl md:text-3xl uppercase">
           The best legal party in town!
         </h2>
         <p className="text-sm md:text-base italic">
           A benefit that supports San Diego legal charities
         </p>
+        <div className="w-screen mx-auto md:float-right md:px-24">
+          <img
+            src={invitationSponsor}
+            className="mx-auto md:float-right"
+            alt="invite text"
+          />
+        </div>
       </div>
     </motion.div>
-  )
+  );
 }
-
--- src/components/When.tsx
+++ src/components/When.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 import info from "/when/info.png";
 import button from "/when/button.png";
 import collage from "/when/collage.png";
 export default function When() {
   return (
-    <motion.div initial={{x: "-100%", opacity: 0}} whileInView={{x: "0", opacity: 1}} transition={{duration: 1}} className='bg-white flex md:w-fit p-4 flex-col md:flex-row mx-auto gap-8 md:gap-0 w-10/12 items-center mt-24'>
-      <div className='flex flex-col items-center'>
+    <motion.div
+      initial={{ x: "-100%", opacity: 0 }}
+      whileInView={{ x: "0", opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="bg-white flex md:w-fit p-4 flex-col md:flex-row mx-auto gap-8 md:gap-0 w-10/12 items-center mt-24 shadow-2xl rounded-lg mb-24"
+    >
+      <div className="flex flex-col items-center">
         <div className="flex-initial">
           <img src={info} alt="info" />
         </div>
-        <div className="">
-          <a href="#" target='_blank'><img src={button} alt="register button" /></a>
-        </div>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <a href="#" target="_blank" className="hover:cursor-pointer">
+            <img src={button} alt="register button" />
+          </a>
+        </motion.div>
       </div>
       <div>
         <img src={collage} alt="collage" />
