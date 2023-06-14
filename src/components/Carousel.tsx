d8352ecce5dd364bf35c627faeeceef683d500b1
Greyson Stalcup <g_stalcup@hotmail.com>
2023-06-14 15:41:37 -0700

    fix error with client load

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,26 +3,28 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { motion } from "framer-motion";
+
 // The carousel images
-import image1 from "/images/qcc002.jpg";
-import image2 from "/images/qcc059.jpg";
-import image3 from "/images/qcc139.jpg";
-import image4 from "/images/qcc154.jpg";
-import image5 from "/images/qcc222.jpg";
-import image6 from "/images/qcc234.jpg";
-import image7 from "/images/qcc239.jpg";
-import image8 from "/images/qcc280.jpg";
+import qcc002 from "/images/qcc002.jpg";
+import qcc059 from "/images/qcc059.jpg";
+import qcc139 from "/images/qcc139.jpg";
+import qcc154 from "/images/qcc154.jpg";
+import qcc222 from "/images/qcc222.jpg";
+import qcc234 from "/images/qcc234.jpg";
+import qcc239 from "/images/qcc239.jpg";
+import qcc280 from "/images/qcc280.jpg";
 
 const imageList = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
+  qcc002,
+  qcc059,
+  qcc139,
+  qcc154,
+  qcc222,
+  qcc234,
+  qcc239,
+  qcc280,
 ];
+
 export default function Carousel() {
   var settings = {
     dots: true,
@@ -33,7 +35,7 @@ export default function Carousel() {
     autoplay: true,
     autoplaySpeed: 4000,
     cssEase: "ease-in-out",
-    showArrows: true,
+    arrows: true, // Change from `showArrows` to `arrows`
   };
 
   return (
@@ -47,7 +49,7 @@ export default function Carousel() {
       <Slider {...settings}>
         {imageList.map((image, index) => (
           <div key={index}>
-            <img src={image} alt="carousel" className="rounded-lg" />
+            <img src={image} alt={`carousel-${index}`} className="rounded-lg" />
           </div>
         ))}
       </Slider>
